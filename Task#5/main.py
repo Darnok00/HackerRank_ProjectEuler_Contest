a7ff550e1bbfd7cff90c9df067bbbbadcadf0adc
Konrad Krzeminski <majunia28@gmail.com>
2023-06-29 23:51:24 +0200

    feat: complete task#5

---
+++ Task#5/main.py
@@ -0,0 +1,39 @@
+"""
+Task #5
+Smallest multiple
+Level: Medium
+Points: 100
+"""
+
+
+def computeGCD(x, y):
+    while y:
+        x, y = y, x % y
+    return abs(x)
+
+
+def computeLCM(t):
+    output_array = []
+
+    for _ in range(t):
+        n = int(input().strip())
+        if n < 3:
+            output_array.append(n)
+        else:
+            lcm = 2
+            for i in range(3, n+1):
+                gcd = computeGCD(lcm, i)
+                lcm = (lcm * i) // gcd
+            output_array.append(lcm)
+
+    return output_array
+
+
+def print_result(result):
+    for element in result:
+        print(element)
+
+
+t = int(input().strip())
+result = computeLCM(t)
+print_result(result)
