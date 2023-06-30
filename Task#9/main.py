bc2ad273acdc9755a9c76c1b1d1bcf43725a7cc8
Konrad Krzeminski <majunia28@gmail.com>
2023-06-30 18:25:15 +0200

    feat: task#1-10 completed

--- Task#9/main.py
+++ Task#9/main.py
@@ -10,15 +10,22 @@ import math
 MAX_SIZE = 3000
 
 
+def print_result(result):
+    for element in result:
+        print(element)
+
+
 def find_largest_product(t):
     output_array = []
     pythagorean_triples = []
+
     for i in range((MAX_SIZE // 3) * 2):
         for j in range(i - 1, 0, -1):
             a_2 = i ** 2 - j ** 2
             a = math.sqrt(a_2)
             if abs(round(a, 0) - a) == 0:
                 pythagorean_triples.append([i, j, int(round(a, 0))])
+
     for _ in range(t):
         n = int(input().strip())
         max_product = -1
@@ -30,11 +37,6 @@ def find_largest_product(t):
         output_array.append(max_product)
 
     return output_array
-
-
-def print_result(result):
-    for element in result:
-        print(element)
 
 
 t = int(input().strip())
